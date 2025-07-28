743826a63bc1082a7a70cf79c6e5475177129bf1
bf39L <boliu1234@gmail.com>
2025-07-28 15:13:20 +1200

    update script

--- karabiner-elements/scripts/cycle_profile.sh
+++ karabiner-elements/scripts/cycle_profile.sh
@@ -1,11 +1,34 @@
+#!/bin/bash
+
 # path: $HOME/.config/karabiner/cycle_profile.sh
 # chmod +x cycle_profile.sh
 
-if test "$(/Library/Application\ Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli --show-current-profile-name)" == "bf39L"; then
-    /Library/Application\ Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli --select-profile "colemak" &&
-        osascript -e 'display notification "colemak" with title "Karabiner" subtitle "Profile Switch" sound name "Purr"'
-    # /opt/homebrew/bin/terminal-notifier -title "Karabiner" -group "Karabiner" -message "Profile: colemak"
-else
-    /Library/Application\ Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli --select-profile "bf39L" &&
-        osascript -e 'display notification "bf39L" with title "Karabiner" subtitle "Profile Switch" sound name "Purr"'
-fi
+# Fail immediately if any command fails
+set -e
+
+# Set PATH to include necessary directories
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+
+# Get current profile
+current_profile=$(karabiner_cli --show-current-profile-name)
+
+# Get all profiles
+profiles=($(karabiner_cli --list-profile-names))
+
+# Find current profile index
+for i in "${!profiles[@]}"; do
+    if [[ "${profiles[$i]}" == "$current_profile" ]]; then
+        current_index=$i
+        break
+    fi
+done
+
+# Get next index (wrap around if at end)
+next_index=$(( (current_index + 1) % ${#profiles[@]} ))
+next_profile="${profiles[$next_index]}"
+
+# Switch to next profile
+karabiner_cli --select-profile "$next_profile"
+
+# Notify user with sound
+osascript -e "display notification \"$next_profile\" with title \"Karabiner\" subtitle \"Profile Switch\" sound name \"Purr\""
